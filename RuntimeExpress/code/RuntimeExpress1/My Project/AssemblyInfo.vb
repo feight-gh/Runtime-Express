cdf3f0b26979ee0640d4ce9711ab672b4aec2722
feight-github <feiyuanhang@126.com>
2014-08-10 16:23:45 +0800

    1.4 Pubilshed
    Added Something
    Fixed Something

---
+++ RuntimeExpress/code/RuntimeExpress1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 查看程序集特性的值
+
+<Assembly: AssemblyTitle("Runtime Express")> 
+<Assembly: AssemblyDescription("一款运行库集成安装器")> 
+<Assembly: AssemblyCompany("Feight Software")> 
+<Assembly: AssemblyProduct("Runtime Express")> 
+<Assembly: AssemblyCopyright("Copyright © Feight Soft 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("ad9f6907-3d55-4ee2-b467-4136110573eb")> 
+
+' 程序集的版本信息由下面四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+' 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”: 
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.4.7.2")> 
+<Assembly: AssemblyFileVersion("1.3.1.0")> 
