96eb97c36d887767fc690da134d04f89c9f643f0
feightwywx <feiyuanhang@126.com>
2016-03-13 23:12:49 +0800

    REXAML 2.0.0.100
    First Blood

--- Windows/code/RuntimeExpress2/Main.Designer.vb
+++ Windows/code/RuntimeExpress2/Main.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Main
     Inherits System.Windows.Forms.Form
 
     'Form 重写 Dispose，以清理组件列表。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Main
     '注意: 以下过程是 Windows 窗体设计器所必需的
     '可以使用 Windows 窗体设计器修改它。  
     '不要使用代码编辑器修改它。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
         Me.TitleLogo = New System.Windows.Forms.PictureBox()
@@ -86,6 +86,7 @@ Partial Class Main
         '
         Me.RuntimeSelector.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.RuntimeSelector.CheckBoxes = True
+        Me.RuntimeSelector.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.RuntimeSelector.Location = New System.Drawing.Point(7, 4)
         Me.RuntimeSelector.Name = "RuntimeSelector"
         Me.RuntimeSelector.Size = New System.Drawing.Size(251, 323)
--- Windows/code/RuntimeExpress2/Main.vb
+++ Windows/code/RuntimeExpress2/Main.vb
@@ -1,6 +1,4 @@
-﻿Imports System.Diagnostics.FileVersionInfo
-
-Public Class Main
+﻿Public Class Main
 
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles Me.Load
 
@@ -24,6 +22,7 @@ Public Class Main
         Else osbit = "32位"
         End If
 
+        '判断系统版本
         Select Case osver
 
             Case "6.0"
@@ -44,13 +43,67 @@ Public Class Main
 
         End Select
 
+        '判断系统是32位还是64位
         If is64bit = False Then
             atTargetCombo.SelectedIndex = 0
         ElseIf is64bit = True Then
             atTargetCombo.SelectedIndex = 1
         End If
 
-        InfoLabel.Text = osfullname & " " & osbit
+        InfoLabel.Text = osfullname & " " & osbit '输出
+
+        initTree()
+
+    End Sub
+
+#Region "节点声明"
+
+    '.net Framework begin
+    Dim dotnet35node = New TreeNode(".net Framework 3.5")
+    'Visual C++ begin
+    Dim vcroot = New TreeNode("Visual C++")
+    Dim vc20051 = New TreeNode("Visual C++ 2005 x86")
+    Dim vc20052 = New TreeNode("Visual C++ 2005 x64")
+    Dim vc20081 = New TreeNode("Visual C++ 2008 x86")
+    Dim vc20082 = New TreeNode("Visual C++ 2008 x64")
+    Dim vc20101 = New TreeNode("Visual C++ 2010 x86")
+    Dim vc20102 = New TreeNode("Visual C++ 2010 x64")
+    Dim vc20131 = New TreeNode("Visual C++ 2013 x86")
+    Dim vc20132 = New TreeNode("Visual C++ 2013 x64")
+    Dim vc20151 = New TreeNode("Visual C++ 2015 x86")
+    Dim vc20152 = New TreeNode("Visual C++ 2015 x64")
+    'JRE begin
+    Dim jreroot = New TreeNode("Java Runtime Environment")
+    Dim jre71 = New TreeNode("Java 7 Update 79 x86")
+    Dim jre72 = New TreeNode("Java 7 Update 79 x64")
+    Dim jre81 = New TreeNode("Java 8 Update 73 x86")
+    Dim jre82 = New TreeNode("Java 8 Update 73 x64")
+    'DirectX begin
+    Dim dx9c = New TreeNode("DirectX 9.0c")
+    'xna begin
+    Dim xnaroot = New TreeNode("Microsoft XNA Framework")
+    Dim xna2 = New TreeNode("Microsoft XNA Framework 2")
+    Dim xna31 = New TreeNode("Microsoft XNA Framework 3.1")
+    Dim xna4 = New TreeNode("Microsoft XNA Framework 4")
+    'Microsoft GFW
+    'OpenAL begin
+    Dim openal = New TreeNode("OpenAL 2.0.7")
+    'x# begin
+    Dim jsharp = New TreeNode(".net J#")
+    Dim fsharp = New TreeNode(".net F#")
+
+#End Region
+
+    Public Sub initTree()
+
+        RuntimeSelector.Nodes.Add(dotnet35node)
+
+        Select Case atTargetCombo.SelectedIndex
+            Case 0
+
+            Case 1
+
+        End Select
 
     End Sub
 
