15a60e0f45b2da1ffb694be8cc3b5f1fb5675f14
William Fei <feiyuanhang@126.com>
2016-07-20 18:24:07 +0800

    20160720
    R更新器和init部分
    +一些动画效果

--- Windows/code/RuntimeExpressXAML/MainWindow.xaml.vb
+++ Windows/code/RuntimeExpressXAML/MainWindow.xaml.vb
@@ -1,3 +1,48 @@
-﻿Class MainWindow
+﻿Imports System.Threading
+
+Class MainWindow
+
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+
+        Dim osVersion As String = Environment.OSVersion.ToString.Substring(0, 24).ToString
+        Dim osTarget As Short = Runtime.InteropServices.Marshal.SizeOf(IntPtr.Zero) * 8
+        Select Case osVersion
+            Case "Microsoft Windows NT 6.0"
+                cbOSver.SelectedIndex = 0
+
+            Case "Microsoft Windows NT 6.1"
+                cbOSver.SelectedIndex = 1
+
+            Case "Microsoft Windows NT 6.2" '以后来补充Windows 10的支持
+                cbOSver.SelectedIndex = 2
+
+        End Select
+
+        Select Case osTarget
+            Case 32
+                cbOStarget.SelectedIndex = 0
+
+            Case 64
+                cbOStarget.SelectedIndex = 1
+
+        End Select
+
+        labelStat.Content = "就绪。程序版本" & My.Application.Info.Version.ToString & "。"
+
+    End Sub
+
+    Private Sub btnCheckUpdate_Click(sender As Object, e As RoutedEventArgs) Handles btnCheckUpdate.Click
+
+        Dim up As updater = New updater()
+        Dim updateActivity As New Thread(AddressOf up.check)
+        updateActivity.Start()
+
+    End Sub
+
+    Private Sub btnHomepage_Click(sender As Object, e As RoutedEventArgs) Handles btnHomepage.Click
+
+        Process.Start("http://feightwywx.github.io/Runtime-Express/")
+
+    End Sub
 
 End Class
---
+++ Windows/code/RuntimeExpressXAML/updater.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Threading
+
+Public Class updater
+
+    Public Property isNewest() As Boolean
+
+    Public Sub check()
+
+        Dim UpdateChannel As String = "Release" '定义当前所使用的软件版本
+        Dim checkserver As String
+
+        Try
+
+            Select Case UpdateChannel
+
+                Case "Release"
+                    checkserver = "https://raw.githubusercontent.com/feight-github/Runtime-Express/re-2/Windows/version.xml"
+
+                    Dim doc As New Xml.XmlDocument
+                    doc.Load(checkserver.Trim)
+                    Dim re As Xml.XmlNodeReader = New Xml.XmlNodeReader(doc)
+                    Dim xmlnewestver As String
+                    Dim xmlnewestdate As String
+                    Dim xmlnewestinfo As String
+                    Dim name As String
+                    '进行一系列的定义
+
+                    While re.Read
+
+                        Select Case re.NodeType
+
+                            Case Xml.XmlNodeType.Element
+                                name = re.Name
+
+                            Case Xml.XmlNodeType.Text
+                                If name.Equals("edition") Then
+                                    xmlnewestver = re.Value
+                                End If
+
+                                If name.Equals("buildtime") Then
+                                    xmlnewestdate = re.Value
+                                End If
+
+                                If name.Equals("whatsnew") Then
+                                    xmlnewestinfo = re.Value
+                                End If
+
+                        End Select
+
+                    End While
+                    '循环获取服务器端的版本信息
+
+                    If xmlnewestver = My.Application.Info.Version.ToString Then
+
+                        isNewest = True
+
+                    ElseIf xmlnewestver <> My.Application.Info.Version.ToString Then
+
+                        isNewest = False
+
+                        If MsgBox("检测到新版本：" & xmlnewestver & vbCrLf &
+                                   "更新时间：" & xmlnewestdate & vbCrLf &
+                                   "更新内容：" & xmlnewestinfo & vbCrLf &
+                                   "要更新吗？",
+                                    MsgBoxStyle.Question + MsgBoxStyle.OkCancel, "检测到新版本") = MsgBoxResult.Ok _
+                                Then Process.Start("http://pan.baidu.com/s/1o6jULke")
+
+                    End If
+
+                Case "Developer"
+                    MsgBox("你目前处于Developer通道，软件更新不可用。" _
+                        , MsgBoxStyle.Exclamation, "Runtime Express")
+
+            End Select
+
+        Catch ex As Exception
+
+            MsgBox("目前无法连接到更新服务器。请检查你的网络连接，然后再试一次。", MsgBoxStyle.Critical, "Runtime Express")
+            '出于用户考虑没有显示出ex.StackTrace的堆栈信息和ex.Message的信息
+
+        End Try
+
+    End Sub
+
+End Class
