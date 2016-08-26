bc9694cc551fe49a2d248849a7987f26dee6e890
William Fei <feiyuanhang@126.com>
2016-08-26 21:21:31 +0800

    20160826
    +过滤器（Smart模式，VC++）
    =Bug修正

--- Windows/code/RuntimeExpressXAML/MainWindow.xaml.vb
+++ Windows/code/RuntimeExpressXAML/MainWindow.xaml.vb
@@ -41,14 +41,14 @@
         Next rei
 
         For i = 0 To listBoxRt.Items.Count - 1
-                If i <> 0 And i <> 13 And i <> 14 And i <> 18 And i <> 19 And i <> 24 And i <> 25 And i <> 29 And
+            If i <> 0 And i <> 13 And i <> 14 And i <> 18 And i <> 19 And i <> 24 And i <> 25 And i <> 29 And
                     i <> 30 And i <> 33 And i <> 34 Then
-                    listBoxRt.Items.Item(i).IsChecked = False
-                    listBoxRt.Items.Item(i).IsEnabled = True
+                listBoxRt.Items.Item(i).IsChecked = False
+                listBoxRt.Items.Item(i).IsEnabled = True
 
-                End If
+            End If
 
-            Next i
+        Next i
 
     End Sub
 
@@ -93,7 +93,7 @@
         cleanAll()
 
         Select Case cbOStarget.SelectedIndex
-            Case 0
+            Case 0 '32位判断块
                 ck2005x64.IsEnabled = False
                 ck2008x64.IsEnabled = False
                 ck2010x64.IsEnabled = False
@@ -104,26 +104,80 @@
                 ckjre8x64.IsEnabled = False
                 ckxmlc6x64.IsEnabled = False
 
+                '查找VC++ 2005/2008（WinSxS索引）
                 If ckSmart.IsChecked = True Then
+                    If IO.Directory.Exists(winpath & "\winsxs\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.4940_none_d08cc06a442b34fc") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.6195_none_d09154e044272b9a") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.762_none_10b2f55f9bffb8f8") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.8428_none_d08a11e2442dc25d") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.91_none_db5f5c9d98cb161f") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.9268_none_d08e1538442a243e") = False Then
+                        ck2005x86.IsChecked = True
+                    End If
+
+                    If IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.1_none_e163563597edeada") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4148_none_5090ab56bcba71c2") = False And
+                        IO.Directory.Exists(winpath & "\winsxs\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4940_none_50916076bcb9a742") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.6161_none_50934f2ebcb7eb57") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.8387_none_5094ca96bcb6b2bb") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.9247_none_5090cb78bcba4a35") = False Then
+                        ck2008x86.IsChecked = True
+                    End If
+
+                    '查找VC++ 2010/2012/2013/2015
                     If IO.File.Exists(syspath & "\msvcp100.dll") = False Then ck2010x86.IsChecked = True
-                    If IO.File.Exists(syspath & "\msvcr100.dll") = False Then ck2010x86.IsChecked = True
                     If IO.File.Exists(syspath & "\msvcp110.dll") = False Then ck2012x86.IsChecked = True
-                    If IO.File.Exists(syspath & "\msvcr110.dll") = False Then ck2012x86.IsChecked = True
                     If IO.File.Exists(syspath & "\msvcp120.dll") = False Then ck2013x86.IsChecked = True
-                    If IO.File.Exists(syspath & "\msvcr120.dll") = False Then ck2013x86.IsChecked = True
                     If IO.File.Exists(syspath & "\msvcp140.dll") = False Then ck2015x86.IsChecked = True
 
                 End If
 
-            Case 1
+            Case 1 '64位判断块
                 If ckSmart.IsChecked = True Then
+                    '查找VC++ 2005/2008（WinSxS索引）
+                    If IO.Directory.Exists(winpath & "\winsxs\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.4940_none_d08cc06a442b34fc") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.6195_none_d09154e044272b9a") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.762_none_10b2f55f9bffb8f8") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.8428_none_d08a11e2442dc25d") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.91_none_db5f5c9d98cb161f") = False AndAlso
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.9268_none_d08e1538442a243e") = False Then
+                        ck2005x86.IsChecked = True
+                    End If
+
+                    If IO.Directory.Exists(winpath & "\winsxs\amd64_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.4940_none_88df89932faf0bf6") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\amd64_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.762_none_c905be8887838ff2") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\amd64_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.8428_none_88dcdb0b2fb19957") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\amd64_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.9268_none_88e0de612fadfb38") = False Then
+                        ck2005x64.IsChecked = True
+                    End If
+
+                    If IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.1_none_e163563597edeada") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4148_none_5090ab56bcba71c2") = False And
+                        IO.Directory.Exists(winpath & "\winsxs\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4940_none_50916076bcb9a742") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.6161_none_50934f2ebcb7eb57") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.8387_none_5094ca96bcb6b2bb") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.9247_none_5090cb78bcba4a35") = False Then
+                        ck2008x86.IsChecked = True
+                    End If
+
+                    If IO.Directory.Exists(winpath & "\WinSxS\amd64_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.1_none_99b61f5e8371c1d4") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\amd64_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4148_none_08e3747fa83e48bc") = False And
+                        IO.Directory.Exists(winpath & "\winsxs\amd64_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4940_none_08e4299fa83d7e3c") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\amd64_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.6161_none_08e61857a83bc251") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\amd64_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.8387_none_08e793bfa83a89b5") = False And
+                        IO.Directory.Exists(winpath & "\WinSxS\amd64_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.9247_none_08e394a1a83e212f") = False Then
+                        ck2008x64.IsChecked = True
+                    End If
+
+                    '查找VC++ 2010/2012/2013/2015
                     If IO.File.Exists(sys32path & "\msvcp100.dll") = False Then ck2010x86.IsChecked = True
-                    If IO.File.Exists(sys32path & "\msvcr100.dll") = False Then ck2010x86.IsChecked = True
+                    If IO.File.Exists(syspath & "\msvcr100.dll") = False Then ck2010x64.IsChecked = True
                     If IO.File.Exists(sys32path & "\msvcp110.dll") = False Then ck2012x86.IsChecked = True
-                    If IO.File.Exists(sys32path & "\msvcr110.dll") = False Then ck2012x86.IsChecked = True
+                    If IO.File.Exists(syspath & "\msvcr110.dll") = False Then ck2012x64.IsChecked = True
                     If IO.File.Exists(sys32path & "\msvcp120.dll") = False Then ck2013x86.IsChecked = True
-                    If IO.File.Exists(sys32path & "\msvcr120.dll") = False Then ck2013x86.IsChecked = True
+                    If IO.File.Exists(syspath & "\msvcr120.dll") = False Then ck2013x64.IsChecked = True
                     If IO.File.Exists(sys32path & "\msvcp140.dll") = False Then ck2015x86.IsChecked = True
+                    If IO.File.Exists(syspath & "\msvcp140.dll") = False Then ck2015x64.IsChecked = True
 
                 End If
 
